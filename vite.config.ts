8f9dc05d46dda4f34841650f17b1cb06f02ae96f
teamlinkedleader <mike@linkedleaders.com>
2026-07-25 10:01:12 -0600

    Fix Vite port conflict

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
